94d137d6f33b58e8f4bf19dfc22d836f105d4e19
MrBrain-YT <109616511+MrBrain-YT@users.noreply.github.com>
2023-03-05 18:10:47 +0300

    Player and accounts update
    Add:
    - player 3d model
    - player animations
    - updated marker 3d model
    - camera zoom
    - creating and enter accounts

---
+++ FarmAmbar/Assets/Scenes/Scripts/CameraZoom.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private int MaxScroll = 11;
+    private int MinScroll = 0;
+    private int CurentScroll = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the amount the mouse wheel has been scrolled
+        float scrollDelta = Input.mouseScrollDelta.y;
+        print(scrollDelta);
+
+        // Zoom the camera in or out depending on the scroll direction
+        if (scrollDelta < 0)
+        {
+            if (CurentScroll < MaxScroll)
+            {
+                transform.position = new Vector3(transform.position.x + 10f, transform.position.y + 10f, transform.position.z);
+                CurentScroll ++;
+            }
+        }
+        else if (scrollDelta > 0)
+        {
+            if (CurentScroll > MinScroll)
+            {
+                transform.position = new Vector3(transform.position.x - 10f, transform.position.y - 10f, transform.position.z);
+                CurentScroll--;
+            }
+        }
+    }
+}
---
+++ players.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 28 2023 г., 13:36
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `players`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `player`
+--
+
+CREATE TABLE `player` (
+  `Name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Password` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `player`
+--
+
+INSERT INTO `player` (`Name`, `Password`) VALUES
+('Suleiman​', 'efsfsefsfsef'),
+('Suleiman​', 'иди работай'),
+('Vasia​', 'иди работай'),
+('123​', '123'),
+('as​', 'as'),
+('Kas120920055002!!! == True  ​', 'Kas1209');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
