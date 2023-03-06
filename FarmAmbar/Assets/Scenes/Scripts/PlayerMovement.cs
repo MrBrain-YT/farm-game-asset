db8d7d5f32b1b780e805171eb5ca1be5a9f87e5b
MrBrain-YT <109616511+MrBrain-YT@users.noreply.github.com>
2023-03-06 08:43:33 +0300

    Mining wood update
    Add:
    - beta model tree
    - animation mining tree
    - mining tree

--- FarmAmbar/Assets/Scenes/Scripts/PlayerMovement.cs
+++ FarmAmbar/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //public GameObject targetObject;
-    public float moveSpeed = 200f;
+    public float moveSpeed = 5f;
+    public Animator Anim;
+    public GameObject Axe;
+
+    int Rotate = 0;
 
     private Transform characterTransform;
     private Transform targetTransform;
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.SetInt("GetWood", 0);
         characterTransform = transform.GetComponent<Transform>();
         targetTransform = GameObject.Find("Cylinder(Clone)").GetComponent<Transform>();
     }
@@ -19,20 +24,75 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetTransform = GameObject.Find("Cylinder(Clone)").GetComponent<Transform>();
-        float distance = Vector3.Distance(characterTransform.position, targetTransform.position);
+        if (PlayerPrefs.GetInt("GetWood") == 0)
+        {
+            Anim.SetFloat("GetWood", 0);
+            GameObject MoveMarker = GameObject.Find("Cylinder(Clone)");
+            if (MoveMarker != null)
+            {
+                targetTransform = GameObject.Find("Cylinder(Clone)").GetComponent<Transform>();
+                float distance = Vector3.Distance(characterTransform.position, new Vector3(targetTransform.position.x, 100.1f, targetTransform.position.z));
 
-        // Move the character towards the target object if it is not yet close enough
-        if (distance > 0.1f)
-        {
-            print(distance);
-            Vector3 direction = (targetTransform.position - characterTransform.position).normalized;
-            characterTransform.position += direction * moveSpeed * Time.deltaTime;
-            //Destroy(GameObject.Find("Cylinder(Clone)"));
-        }
-        else
-        {
-            Destroy(GameObject.Find("Cylinder(Clone)"));
+                // Move the character towards the target object if it is not yet close enough
+                if (distance > 2f)
+                {
+                    transform.LookAt(new Vector3(targetTransform.position.x, targetTransform.position.y - 12.5f, targetTransform.position.z));
+                    if (Rotate == 0)
+                    {
+
+
+                        //transform.rotation = Quaternion.Euler(0, transform.rotation.y, transform.rotation.z);
+                        Rotate = 1;
+                    }
+                    //transform.LookAt(targetTransform);
+                    Anim.SetFloat("Wolk", 1f);
+                    print(distance);
+                    Vector3 direction = (targetTransform.position - characterTransform.position).normalized;
+                    characterTransform.position += direction * moveSpeed * Time.deltaTime;
+                    //Destroy(GameObject.Find("Cylinder(Clone)"));
+                }
+                else
+                {
+                    Anim.SetFloat("Wolk", 0f);
+                    Destroy(GameObject.Find("Cylinder(Clone)"));
+                    Rotate = 0;
+                }
+            }
+            GameObject TreeMarker = GameObject.Find("Sphere(Clone)");
+            if (TreeMarker != null)
+            {
+                Transform targetTransformTree = GameObject.Find("Sphere(Clone)").GetComponent<Transform>();
+                float distance2 = Vector3.Distance(characterTransform.position, new Vector3(targetTransformTree.position.x, 100.1f, targetTransformTree.position.z));
+
+                // Move the character towards the target object if it is not yet close enough
+                if (distance2 > 5.5f)
+                {
+                    transform.LookAt(new Vector3(targetTransformTree.position.x, targetTransformTree.position.y - 0.5f, targetTransformTree.position.z));
+                    if (Rotate == 0)
+                    {
+
+
+                        //transform.rotation = Quaternion.Euler(0, transform.rotation.y, transform.rotation.z);
+                        Rotate = 1;
+                    }
+                    //transform.LookAt(targetTransform);
+                    Anim.SetFloat("Wolk", 1f);
+                    print(distance2);
+                    Vector3 direction2 = (targetTransformTree.position - characterTransform.position).normalized;
+                    characterTransform.position += direction2 * moveSpeed * Time.deltaTime;
+                    //Destroy(GameObject.Find("Cylinder(Clone)"));
+                }
+                else
+                {
+                    this.GetComponent<MiningWood>().time = 0;
+                    PlayerPrefs.SetInt("GetWood", 1);
+                    Anim.SetFloat("Wolk", 0f);
+                    Axe.SetActive(true);
+                    Anim.SetFloat("GetWood", 1f);
+                    Destroy(GameObject.Find("Cylinder(Clone)"));
+                    Rotate = 0;
+                }
+            }
         }
     }
 }
--- FarmAmbar/Assets/Scenes/Scripts/createMarcer.cs
+++ FarmAmbar/Assets/Scenes/Scripts/createMarcer.cs
@@ -6,37 +6,82 @@ public class createMarcer : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Marcer;
+    public GameObject TreeMarcer;
+    public GameObject DelTree;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit2;
+        if (Physics.Raycast(ray2, out hit2))
         {
-            GameObject obj = GameObject.Find("Cylinder(Clone)");
-            if (obj == null)
+            if (hit2.collider.gameObject.name == "Tree")
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
+                DelTree = hit2.collider.gameObject;
+                if (Input.GetMouseButtonDown(1))
                 {
-                    // Get the point of contact between the ray and the object
-                    Vector3 contactPoint = hit.point;
+                    GameObject obj = GameObject.Find("Sphere(Clone)");
+                    if (obj == null)
+                    {
+                        Destroy(GameObject.Find("Cylinder(Clone)"));
+                        Instantiate(TreeMarcer, new Vector3(hit2.point.x, 100.4f, hit2.point.z), Quaternion.identity);
+                    }
+                    else
+                    {
+                        Destroy(GameObject.Find("Cylinder(Clone)"));
+                        Destroy(GameObject.Find("Sphere(Clone)"));
+                        Instantiate(TreeMarcer, new Vector3(hit2.point.x, 100.4f, hit2.point.z), Quaternion.identity);
+                    }
+                }
+            }
+            else
+            {
+                //Transform targetTransform = GameObject.Find("Cylinder(Clone)").GetComponent<Transform>();
+                //GameObject.Find("Farmer").transform.LookAt(new Vector3(targetTransform.position.x, targetTransform.position.y - 11f, targetTransform.position.z));
+                if (Input.GetMouseButtonDown(1))
+                {
+                    GameObject obj = GameObject.Find("Cylinder(Clone)");
+                    if (obj == null)
+                    {
+                        Destroy(GameObject.Find("Sphere(Clone)"));
+                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            // Get the point of contact between the ray and the object
+                            Vector3 contactPoint = hit.point;
 
-                    // Do something with the contact point, such as log it to the console
+                            // Do something with the contact point, such as log it to the console
 
-                    Debug.Log("Contact point: " + contactPoint);
-                    Instantiate(Marcer, new Vector3(contactPoint.x, 110.6f, contactPoint.z), Quaternion.identity);
+                            Debug.Log("Contact point: " + contactPoint);
+                            Instantiate(Marcer, new Vector3(contactPoint.x, 112.79f, contactPoint.z), Quaternion.identity);
+                        }
+                    }
+                    else
+                    {
+                        Destroy(GameObject.Find("Cylinder(Clone)"));
+                        Destroy(GameObject.Find("Sphere(Clone)"));
+                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            // Get the point of contact between the ray and the object
+                            Vector3 contactPoint = hit.point;
+
+                            // Do something with the contact point, such as log it to the console
+
+                            Debug.Log("Contact point: " + contactPoint);
+                            Instantiate(Marcer, new Vector3(contactPoint.x, 112.79f, contactPoint.z), Quaternion.identity);
+                        }
+                    }
                 }
             }
         }
-        if (Input.GetMouseButton(0))
-        {
-            
-            
-        }
     }
 }
+
